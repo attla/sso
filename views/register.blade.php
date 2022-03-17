518e2da0af59cebd10b8a2127136a2b52733943b
Lucas Nicolau <lucas@octha.com>
2022-03-16 23:27:21 -0300

    ✨ adding redirect location parameter

--- views/register.blade.php
+++ views/register.blade.php
@@ -69,7 +69,7 @@
 							</div>
 						</form>
 						<div class="text-center">
-							<span>Já possui conta? <a href="@route(config('sso.route-group.as') . 'login', ['token' => $token])">Acesse sua conta</a>.</span>
+							<span>Já possui conta? <a href="@route(config('sso.route-group.as') . 'login', ['token' => $token, 'redirect' => $redirect])">Acesse sua conta</a>.</span>
 						</div>
 					</div>
 				</div>
